7050c56a16c8dd7b4462f52bd227d37e04e1e721
littlefire <aybwhite@gmail.com>
2021-02-16 19:05:39 -0800

    added values to enter into burger table through seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('veggie burger', true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('crabby patty', true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('impossible burger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('big mac', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('buttermilk fried chicken burger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('double decker beef burger', false);
