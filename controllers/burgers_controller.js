be8b8c9ef5b04a8151aee2200b46aa8919665db3
littlefire <aybwhite@gmail.com>
2021-02-17 14:47:37 -0800

    finished burgers_controller.js-connection successful- problem with HTML routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -25,3 +25,37 @@ router.post("/api/burgers", function(req,res) {
     });
 });
 
+//API devour > devoured 
+router.put("/api/burgers/:id", function(req,res) {
+    const condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.updateOne(
+        {
+            devoured: req.body.devoured
+        },
+        condition,
+        function(result) {
+            if(result.changeRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        }
+    );
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.deleteOne(condition, function(result) {
+        if (result.affectedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+// Export routes for server.js to use
+module.exports = router;
