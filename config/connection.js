a2a8b6b6e9572c9614121f8207b991904751c237
littlefire <aybwhite@gmail.com>
2021-02-20 15:55:33 -0800

    heroku

--- config/connection.js
+++ config/connection.js
@@ -8,11 +8,11 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "d6rii63wp64rsfb5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        host: "hwr4wkxs079mtb19.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
         port: 3306,
-        user: "a6zekygwk8p3afw7",
-        password: "pf0160wgwfvh2sk3",
-        database: "ub7urmo990w2f5ke"
+        user: "	xffrwiiy26yt7aqn",
+        password: "siq7vcoocdfv15ih",
+        database: "ntdi7cfy9yd3je4q"
     });
 }
 
