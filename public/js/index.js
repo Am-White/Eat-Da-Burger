abe3a03811f2b1d8018e2112aeda45c19ae64250
littlefire <aybwhite@gmail.com>
2021-02-19 21:44:44 -0800

    index.js ajaxs functions added - shouldve done earlier

---
+++ public/js/index.js
@@ -0,0 +1,50 @@
+$(function() {
+    $(".change-devoured").on("click", function(event) {
+        var id = $(this).data("id");
+        var newDevoured = $(this).data("devoured");
+        var newDevouredState = {
+            devoured: newDevoured
+        };
+
+        $.ajax("/api/burger/" + id, {
+            type: "PUT",
+            data: newDevouredState
+        }).then(
+            function() {
+                console.log("changed devoured to", newDevoured);
+                location.reload();
+            }
+        );
+    });
+    
+    $(".submit-form").on("submit", function(event) {
+        event.preventDefault();
+
+        var newBurger = {
+            burger_name: $("#bg").val().trim(),
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function() {
+                console.log("create new burger");
+                location.reload();
+            }
+        );
+    });
+
+    $(".clear-devoured").on("click", function(event) {
+        var id = $(this).data("id");
+
+        $.ajax("/api/burgers/" + id, {
+            type: "DELETE"
+        }).then(
+            function() {
+                console.log("deleted burger", id);
+                location.reload();
+            }
+        );
+    });
+});
