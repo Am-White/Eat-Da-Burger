87ce072c3757126a36a02d748d6f4e82a685b79c
littlefire <aybwhite@gmail.com>
2021-02-24 16:50:39 -0800

    attempt to trouble shoot connection with mysql and heroku

--- config/connection.js
+++ config/connection.js
@@ -8,13 +8,13 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "hwr4wkxs079mtb19.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        host: "d6rii63wp64rsfb5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
         port: 3306,
-        user: "xffrwiiy26yt7aqn",
-        password: "siq7vcoocdfv15ih",
-        database: "ntdi7cfy9yd3je4q"
+        user: "a6zekygwk8p3afw7",
+        password: "pf0160wgwfvh2sk3",
+        database: "ub7urmo990w2f5ke"
     });
-}
+};
 
 // Make connection
 connection.connect(function(err) {
